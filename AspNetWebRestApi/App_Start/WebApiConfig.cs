5364e1cc4bb5ff54ea0697e4973d84f803b9302a
dilara153232@gmail.com <dilara153232@gmail.com>
2022-02-05 22:40:27 +0300

    Configured in web api config file

--- AspNetWebRestApi/App_Start/WebApiConfig.cs
+++ AspNetWebRestApi/App_Start/WebApiConfig.cs
@@ -16,6 +16,14 @@ namespace AspNetWebRestApi
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
+            name: "MyProfil",
+            routeTemplate: "api/myprofil/{id}",
+            defaults: new { controller = "myprofil", id = RouteParameter.Optional },
+            constraints: new { id = "/d+" }
+        );   
+
+
+            config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
